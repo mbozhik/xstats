0579e8ba93c8e2c81033db29eb9555e3cadab5ea
maxim <bozzhik@gmail.com>
2025-11-26 20:08:46 +0300

    create `Container` global component

---
+++ src/components/global/container.tsx
@@ -0,0 +1,11 @@
+import {cn} from '@/lib/utils'
+
+export const BOX = {
+  container: 'px-3 py-3.5 max-w-xl mx-auto',
+  display: 'flex flex-col gap-4.5',
+  offset: 'pt-4 pb-14',
+}
+
+export default function Container({children, className}: {children: React.ReactNode; className?: string}) {
+  return <main className={cn(BOX.container, BOX.display, BOX.offset, 'min-h-screen', className)}>{children}</main>
+}
